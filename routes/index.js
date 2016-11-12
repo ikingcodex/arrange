b3cf775083b71a5d6909ff2802ad01e2fe60088c
ikingcodex <abangelsha@gmail.com>
2016-11-13 00:49:41 +0100

    added new images, fonts and created the explore and profile page

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Arrange'});
 });
+router.get('/explore', function(req, res, next) {
+  res.render('explore', { title: 'Arrange'});
+});
+router.get('/:username', function(req, res, next) {
+  res.render('profile', { title: 'Arrange'});
+});
 
 module.exports = router;
